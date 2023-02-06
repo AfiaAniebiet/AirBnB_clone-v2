4145246f5bc50e2d0c369a32ab10c5b10111fb0a
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-06 13:24:50 -0800

    Script that starts a Flask web application

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+""" Script that starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
